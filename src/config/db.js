bccfd2ef03c4657505880e715659a22f656eae60
Iago Ferreira <iago.ferreira@ops.team>
2024-02-18 15:07:04 -0300

    update files

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 module.exports = new Pool({
   user: 'postgres',
   password: 'password',
-  host: 'postgres',
+  host: 'database-1.cfsausgm8chc.us-east-2.rds.amazonaws.com',
   port: 5432,
   database: 'launchstore'
 });
